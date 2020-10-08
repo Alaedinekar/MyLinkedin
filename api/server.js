bced6e72b1304c5a3bf6642aecfcbd5d9e7b1def
karouia <alaedinekarouia@gmail.com>
2020-10-08 14:09:27 +0200

    test connexion bd

--- api/server.js
+++ api/server.js
@@ -11,4 +11,6 @@ mongoose.connect(DB_URI,{
     else console.log('Connected to the database')
 })
 app.get('/', (req, res) => res.send('Hellow World!'));
+
+app.get('/personne',(req,res)=> res.send(db.maincluster.find({})))//Select * from tablepersonne
 app.listen(port, () => console.log(`Example app listening on port port!`));
