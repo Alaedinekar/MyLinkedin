188cf443200f0ca774f216c0ff3b2d7115017f01
Marco <marc.trentin06@gmail.com>
2020-10-13 17:15:29 +0200

    quelques routes sur user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -6,7 +6,8 @@ const newUser = async (req,res) => {
     const newUser = new userModel({
         userName : data.userName,
         userEmail : data.userEmail,
-        userPhoneNumber : data.userPhoneNumber 
+        userPhoneNumber : data.userPhoneNumber,
+        skillsList : data.skillsList
     });
     await newUser.save()
     .then(data => {
@@ -18,11 +19,37 @@ const newUser = async (req,res) => {
 }
 
 const getUsers = async (req,res) => {
-    const users = await userModel.find();
-    res.json(users);
+    await userModel.find()
+    .then(result => {
+       res.status(200).send(result)
+    })
+    .catch(error => {
+        res.send({message : error.message})
+    })
 }
-
+const getUserByName = async (req,res) => {
+    const name = req.params.name;
+    await userModel.find({userName : name})
+    .then(rslt => {
+        rslt.length ? res.status(200).json(rslt) : res.status(200).json({erreur : "Utilisateur inconnu ...."})
+    })
+    .catch(err => {
+        res.status(400).send({message : err.message});
+    })
+}
+const getBySkill = async (req,res) => {
+    const skillName = req.params.skillName;
+    await userModel.find({skillName : skillName})
+    .then(rslt => {
+        rslt.length ? res.status(200).json(rslt) : res.status(200).json({erreur : "Utilisateur inconnu ...."})
+    })
+    .catch(err => {
+        res.status(400).send({message : err.message});
+    })
+}
 module.exports = {
     newUser : newUser,
-    getUsers : getUsers
+    getUsers : getUsers,
+    getUserByName : getUserByName,
+    getBySkill : getBySkill
 }
