6fcee34f0cf8af041438cde280d6ae14c2710080
Marco <marc.trentin06@gmail.com>
2020-10-17 17:54:48 +0200

    acess control management

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,5 @@
 const userController = require('../controllers/user');
 const express = require('express');
-const user = require('../controllers/user');
 const router = express.Router();
 
 router.post('/addUser', userController.newUser); // créer un nouvel utilisateur (voir le model pour le body)
@@ -8,10 +7,6 @@ router.get('/', userController.getUsers); // renvoi tous les users
 router.get('/name/:name', userController.getUserByName);
 router.get('/id/:id', userController.getUserById);
 router.get('/skill/:skillName', userController.getBySkill);
-<<<<<<< HEAD
-router.post('/skill/add/:id', userController.newSkill);
-module.exports = router;
-=======
 
 module.exports = router;
 
@@ -25,4 +20,3 @@ app.get('/personne',async (req,res)=> {
     }//Select * from tablepersonne
 })
 */
->>>>>>> 23ec36ae8e94388e93fc6e06227e178d0b03a61c
--- api/server.js
+++ api/server.js
@@ -7,10 +7,28 @@ const DB_URI = require('./config/db_config');
 const userRoutes = require('./routes/user');
 const uri = require('./config/db_config');
 
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // Middlewares
 app.use(bodyParser.json());
-app.use('/user', userRoutes); 
+app.use('/user', userRoutes);
 
 // DB connection
 mongoose.connect(DB_URI,{
@@ -21,6 +39,13 @@ mongoose.connect(DB_URI,{
     else console.log('Connected to the database')
 })
 console.log(uri)
+
+// Statics routes
+const listEndpoints = require("express-list-endpoints"); // npm i express-list-endpoints
+// console.log(listEndpoints(app)); // where app = express();
+app.get("/routes", (req,res) =>{
+  res.send(listEndpoints(app));
+})
 app.get('/',  (req, res) => res.send('Hellow World!'));
 
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () => console.log(`http://localhost:${port}/`));
